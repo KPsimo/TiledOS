8c92cf89979b2c4d5cf7fe76c3dcf40e24c70701
SmartLayout <114115793+SmartLayout64@users.noreply.github.com>
2025-10-12 11:39:07 -0400

    Create uiData.py

---
+++ data/uiData.py
@@ -0,0 +1,13 @@
+screenWidth = 1920
+screenHeight = 1080
+
+quickScale = 1
+
+cellSize = 110 * quickScale
+cellPadding = 10 * quickScale
+
+backgroundColor = (10, 10, 10)
+widgetBackgroundColor = (30, 30, 30)
+textColor = (255, 255, 255)
+
+cornerRadius = 15
